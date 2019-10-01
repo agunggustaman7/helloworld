4bc1a1d5cde4c7b2dfdec93f11c2106b17f9f45a
agunggustaman7 <agunggustaman29@gmail.com>
2019-10-01 09:09:01 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <div class="app-6">
+      <input v-model="message" placeholder="isi dulu">
+      
+      <button v-on:click="tetcConsole">Submit</button>
+      <p>{{message}}</p>
+    </div> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: ""
+  },
+  data(){
+    return {
+      message:''
+    }
+  },
+  methods:{
+    tetcConsole() {
+      alert(this.message)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
